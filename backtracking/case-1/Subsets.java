27afa64ac02f6f524f494b71b8ebf4afe75826f2
Azamat Bekmuratov <azamat.bekmuratov@gmail.com>
2022-07-14 22:27:48 +0600

    subsets with backtracking technique

--- backtracking/case-1/Subsets.java
+++ backtracking/case-1/Subsets.java
@@ -7,6 +7,7 @@ class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
+        
         helper(ans, 0, nums, list);
         return ans;
     }

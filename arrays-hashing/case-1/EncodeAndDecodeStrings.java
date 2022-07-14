12f334eb0e41b5be91b4af4da9aeb784f5439581
Azamat Bekmuratov <azamat.bekmuratov@gmail.com>
2022-07-14 23:06:39 +0600

    encode and decode string solution

--- arrays-hashing/case-1/EncodeAndDecodeStrings.java
+++ arrays-hashing/case-1/EncodeAndDecodeStrings.java
@@ -13,15 +13,16 @@ class EncodeAndDecodeStrings {
     }
 
     public List<String> decode(String str) {
+        // Input: "4#love5#coding"
         List<String> list = new ArrayList<>();
         int i = 0;
         while (i < str.length()) {
             int j = i;
             while (str.charAt(j) != '#') {
-                j++;
+                j++; // j=1
             }
-            int length = Integer.valueOf(str.substring(i, j));
-            i = j + 1 + length;
+            int length = Integer.valueOf(str.substring(i, j)); // i=0; j=1; length = 4;
+            i = j + 1 + length; // i= 1 + 1 + 4 = 6
             list.add(str.substring(j + 1, i));
         }
         return list;

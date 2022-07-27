83772001706765372cd040d26be47d8ee3edd27b
Azamat Bekmuratov <azamat.bekmuratov@gmail.com>
2022-07-27 16:53:41 +0600

    binary search example

---
+++ two-pointer/case-2/ThreeSum.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class ThreeSum {
+    public int[][] threeSum(int[] numbers, int target) {
+        Arrays.sort(numbers);
+    }
+
+    public void main(String[] args) {
+
+    }
+}

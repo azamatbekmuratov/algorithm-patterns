2ea86572220f19576bc40a8a32a438769cbd0bc1
Azamat Bekmuratov <azamat.bekmuratov@gmail.com>
2022-08-14 00:40:44 +0600

    solution for Top K Frequent Elements in Golang

--- arrays-hashing/top-k-frequent-elements/main.go
+++ arrays-hashing/top-k-frequent-elements/main.go
@@ -1,4 +1,4 @@
-// Top K Frequent Elements
+// Top K Frequent Elements in Golang
 // Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 package main
 

153366b6e795a7f5e7a77e901be6249e40499f67
Azamat Bekmuratov <azamat.bekmuratov@gmail.com>
2022-08-18 23:24:01 +0600

    valid palindrome solution with two pointer

---
+++ two-pointer/valid-palindrome/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func isPalindrome(str string) bool {
+	l, r := 0, len(str)-1
+	for l < r {
+		if !is_alphanum(string(str[l])) {
+			l++
+			continue
+		} else if !is_alphanum(string(str[r])) {
+			r--
+			continue
+		} else if !strings.EqualFold(string(str[l]), string(str[r])) {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
+
+func is_alphanum(word string) bool {
+	return regexp.MustCompile(`^[a-zA-Z0-9]*$`).MatchString(word)
+}
+
+func main() {
+	fmt.Println(isPalindrome("aka"))
+
+}
